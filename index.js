94637c76226ec038faf61847404a41cf66db3882
Mackenzie Craig <hello@maccraig.net>
2020-10-04 18:50:48 -0700

    Added elo

--- index.js
+++ index.js
@@ -5,30 +5,28 @@ const axios = require('axios');
 
 const Player = require('./models/player');
 const { findOne } = require('./models/player');
+const player = require('./models/player');
 
 const app = express()
 const port = 8080
 
-var last = 2705740;
+var GLOBAL_K = 30;
 
 mongoose.connect('mongodb://localhost/ladder', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-  
-  var i = last;
-  
-    setInterval(() => {fetchGame(i); i++}, 1000);
+    fetchGame(2706481);
 
+    // console.log(calculateChange(GLOBAL_K, 1500, 1600));
 })
 
 function fetchGame(id) {
@@ -45,8 +43,56 @@ function fetchGame(id) {
         } else {
             console.log(error);
         }
-        
+
     });
+}
+
+function updatePlayer(player_id3, username, elo_change) {
+    Player.findOneAndUpdate({ id3: player_id3 }, { $inc: { elo: elo_change } }, { new: true }, function (err, player) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (player) {
+                // found player, update in db
+                // console.log("found: ", player);
+            } else {
+                // no player in db
+                //console.log("no player: " + player_id3)
+                var new_player = new Player({
+                    id3: player_id3,
+                    last_username: username,
+                    elo: 1500
+                });
+                new_player.save();
+            }
+        }
+    });
+}
+
+function getPlayer(player_id3, callback) {
+    Player.findOne({ id3: player_id3 }, (err, player_found) => {
+        callback(player_found);
+    })
+}
+
+function calculateProbability(rating, rating_opponent) {
+    return 1 / (1 + Math.pow(10, ((rating - rating_opponent) / 400)));
+}
+
+function calculateChange(K, winnerElo, loserElo) {
+    var probB = calculateProbability(winnerElo, loserElo);
+    var probA = calculateProbability(loserElo, winnerElo);
+
+    console.log(probA);
+    console.log(probB);
+
+    var resultA;
+    var resultB;
+
+    resultA = winnerElo + K * (1 - probA);
+    resultB = loserElo + K * (0 - probB);
+
+    return { 'winner': resultA, 'loser': resultB };
 }
 
 function recordGame(game) {
@@ -54,26 +100,36 @@ function recordGame(game) {
     var players = game.players;
 
     for (player_id3 in players) {
-        
         console.log(player_id3 + " + " + game.names[player_id3]);
+        updatePlayer(player_id3, game.names[player_id3], +30);
+    }
 
+    var teams = game.teams;
 
-        var query = {id3: player_id3}, update = { expire: new Date() }, options = { upsert: true, new: true, setDefaultsOnInsert: true };
-        // Find the document
-        Player.findOneAndUpdate(query, update, options, function(error, result) {
-            if (error) return;
+    var blue_elo = 0;
+    var red_elo = 0;
 
-            // do something with the document
-            console.log(result);
-            var new_player = new Player({
-                id3: player_id3,
-                last_username: game.names[player_id3],
-                elo: 1000
-            });
-        });
+    for (player_id3 in players) {
+        getPlayer(player_id3, (player) => {
 
+            if (game.players[player_id3].team == "Red") {
+                red_elo += player.elo;
+            } else if (game.players[player_id3].team == "Blue") {
+                blue_elo += player.elo;
+            }
 
-
+        })
 
     }
+
+    if (teams.Red.score > teams.Blue.score) {
+        // red won
+        console.log("red won");
+    } else {
+        // blue won
+        console.log("blue won");
+    }
+    console.log("blue elo: " + blue_elo + " red elo: " + red_elo);
+
+
 }
